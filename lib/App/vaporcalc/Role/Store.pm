8fefd2bbbd12a89759171592631895492a32a93a
Jon Portnoy <avenj@cobaltirc.org>
2014-02-04 16:57:42 -0500

    add Role::Store
    	new file:   lib/App/vaporcalc/Role/Store.pm

---
+++ lib/App/vaporcalc/Role/Store.pm
@@ -0,0 +1,32 @@
+package App::vaporcalc::Role::Store;
+use Defaults::Modern;
+
+use JSON::Tiny;
+use Role::Tiny;
+
+method save ( (Str | Path) $path ) {
+  unless ($self->can('TO_JSON')) {
+    confess "No TO_JSON method found for ".blessed $self
+  }
+
+  my $jseng = JSON::Tiny->new;
+  my $json  = $jseng->encode($self);
+  unless ($json) {
+    confess "Could not encode JSON: ".$jseng->error
+  }
+
+  path($path)->spew_utf8($json)
+}
+
+method load ( (Str | Path) $path ) {
+  my $json  = path($path)->slurp_utf8;
+  my $jseng = JSON::Tiny->new;
+  my $data  = $jseng->decode($json);
+  unless ($data) {
+    confess "Could not decode JSON: ".$jseng->error
+  }
+
+  (blessed $self || $self)->new(%$data)
+}
+
+1;
