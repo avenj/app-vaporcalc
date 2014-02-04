12347bcad0f959df014d6254f3f3e3c80699c174
Jon Portnoy <avenj@cobaltirc.org>
2014-02-03 21:54:10 -0500

    cleanups

--- lib/App/vaporcalc.pm
+++ lib/App/vaporcalc.pm
@@ -4,24 +4,6 @@ use Defaults::Modern;
 
 use App::vaporcalc::Recipe;
 
-# Want to plug in:
-#   - base nic         (default 100mg/ml)
-#   - target nic       (default 16mg/ml)
-#   - target quantity  (default 10ml)
-#   - PG %             (default 65% - conv to ml via target qty)
-#   - VG %             (default 35% - conv to ml via target qty)
-#   - Flavor %         (default 20% - conv to ml via target qty)
-#
-# Want to get:
-#   - nic base total ml
-#   - PG total ml
-#   - VG total ml
-#   - flavor total ml
-
-# Feature list:
-#   - recipe creation / storage
-#
-
 use parent 'Exporter::Tiny';
 our @EXPORT = our @EXPORT_OK = 'vcalc';
 
