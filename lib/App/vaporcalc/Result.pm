19fbe217122934911e46f892777cecf5cfe2e263
Jon Portnoy <avenj@cobaltirc.org>
2014-02-05 18:47:40 -0500

    Result; kill unnecessary BUILD, use StrictNum
    	modified:   lib/App/vaporcalc/Result.pm

--- lib/App/vaporcalc/Result.pm
+++ lib/App/vaporcalc/Result.pm
@@ -5,49 +5,36 @@ use Defaults::Modern
 
 use Moo; use MooX::late;
 
+ 
 has vg => (
   required => 1,
   is       => 'ro',
-  isa      => Num,
+  isa      => StrictNum,
 );
 
 has pg => (
   required => 1,
   is       => 'ro',
-  isa      => Num,
+  isa      => StrictNum,
 );
 
 has nic => (
   required => 1,
   is       => 'ro',
-  isa      => Num,
+  isa      => StrictNum,
 );
 
 has flavor => (
   required => 1,
   is       => 'ro',
-  isa      => Num,
+  isa      => StrictNum,
 );
 
 has total => (
   required => 1,
   is       => 'ro',
-  isa      => Num,
+  isa      => StrictNum,
 );
-
-
-method BUILD {
-  my $real_total = $self->vg + $self->pg + $self->nic + $self->flavor;
-  unless ($real_total == $self->total) {
-    carp "Totals do not add up!",
-      "  VG: ", $self->vg, "\n",
-      "  PG: ", $self->pg, "\n",
-      "  NIC: ", $self->nic, "\n",
-      "  FLAV: ", $self->flavor, "\n",
-      "    == $real_total\n", 
-      "  Wanted ", $self->total
-  }
-}
 
 method TO_JSON {
   +{
